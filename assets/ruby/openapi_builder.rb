11ad53edb21f2afc40d18f2b0478b6ca72fb34fe
Shane Curcuru <asf@shanecurcuru.org>
2024-02-24 12:20:50 -0500

    Update foundation list and improve openapi aliases

--- assets/ruby/openapi_builder.rb
+++ assets/ruby/openapi_builder.rb
@@ -9,14 +9,20 @@ module OpenapiBuilder
   require_relative 'foundation_reporter.rb'
 
   # Build api:_foundations/list data structure
+  # TODO replace this with a curated aliasOf feature instead
   # @return hash of foundation ids => array of alternate ids
   def build_list(dir)
     data = FoundationReporter.get_yamldataset(dir)
     list = {}
     data.each do | id, hash |
       list[id] = []
-      list[id] << hash['commonName']
       list[id] << hash['website'].sub(/^https?\:\/\/(www.)?/,'').chomp('/')
+      cn = hash['commonName']
+      ln = hash.fetch('legalName', nil)
+      list[id] << cn
+      if ln && !ln.eql?(cn)
+          list[id] << hash['legalName']
+      end
       # TODO are there any other alternate lookup methods to include?
     end
     return list
